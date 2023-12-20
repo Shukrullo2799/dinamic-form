78f6b70c2228f631ad73e30cbfa86c9679ce256e
Shukrullo2799 <104069212+Shukrullo2799@users.noreply.github.com>
2023-12-20 21:54:33 +0500

    dependencies install

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
 <script setup></script>
 
 <template>
-  <main>Dinamic Form Builder</main>
+  <main class="flex h-screen md:flex-row flex-column">
+    <div class="md:w-6 p-5">
+      <p class="font-500 text-2xl">Поля</p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim consectetur repellat quis fugit
+      doloremque cumque id, obcaecati dignissimos soluta. Quasi architecto expedita sapiente. Quis
+      soluta animi, recusandae cupiditate provident beatae!
+    </div>
+    <div class="bg-primary md:w-6 p-5">
+      result Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos commodi
+      consequuntur, distinctio ducimus ipsum magni quod assumenda fugit incidunt cupiditate facere
+      quo provident, voluptas quae placeat accusantium quibusdam modi animi!
+    </div>
+  </main>
 </template>
